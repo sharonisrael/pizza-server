2865db40c1d5c2829fbd21080b6841e1de0f253c
Sharon Israel <avisar@hotmail.com>
2020-09-26 13:29:58 +0300

    PIzza CRUD

--- api/pizzas_api_express.js
+++ api/pizzas_api_express.js
@@ -12,9 +12,16 @@ pizzaApp.get("/", (req, res) => {
   res.send("Hello pizza");
 });
 
+pizzaApp.get("/api/all_pizzas", (req, res) => {
+  res.status(200).json(pizzaTypes);
+});
+
 // Use http://localhost:5000/api/pizza/100
 pizzaApp.get("/api/pizza/:id", (req, res) => {
   console.log("In pizza id:", req.params.id);
+  // Can be either with find (object) or filter (array)
+  // const pizza = pizzaTypes.find((p) => p.id === parseInt(req.params.id));
+  // if (!pizza) {
   const pizza = pizzaTypes.filter((p) => p.id === parseInt(req.params.id));
   if (pizza.length == 0) {
     res.status(404).send(`Pizza ${req.params.id} not found`);
@@ -23,11 +30,12 @@ pizzaApp.get("/api/pizza/:id", (req, res) => {
   }
 });
 
-pizzaApp.get("/api/all_pizzas", (req, res) => {
-  res.status(200).json(pizzaTypes);
-});
-
 // This is the body in RAW JSON format
+// Make sure in postman you use {..} and not [{..}]
+// Good
+//{"id":101,"name":"H1","image":"/images/pizza_vegetables.jpg","description":"Pizza with vegetables and special sauce","price":50}
+// Bad
+// [{"id":101,"name":"H1","image":"/images/pizza_vegetables.jpg","description":"Pizza with vegetables and special sauce","price":50}]
 // {
 //   id: 103,
 //   name: 'Haim',
@@ -36,11 +44,45 @@ pizzaApp.get("/api/all_pizzas", (req, res) => {
 //   price: 50
 // }
 pizzaApp.post("/api/add_pizza", (req, res) => {
-  console.log(req.body);
   const pizza = req.body;
   pizzaTypes.push(pizza);
+  console.log(pizzaTypes);
   // return the pizza so client will know the id
   res.send(pizza);
+});
+
+// Make sure in postman you use {..} and not [{..}]
+// Good
+//{"id":101,"name":"H1","image":"/images/pizza_vegetables.jpg","description":"Pizza with vegetables and special sauce","price":50}
+// Bad
+// [{"id":101,"name":"H1","image":"/images/pizza_vegetables.jpg","description":"Pizza with vegetables and special sauce","price":50}]
+pizzaApp.put("/api/update_pizza_name/:id", (req, res) => {
+  console.log("BODY", req.body);
+  let pizza = pizzaTypes.find((p) => p.id === parseInt(req.params.id));
+  if (!pizza) {
+    res.status(404).send(`Pizza ${req.params.id} not found`);
+  } else {
+    console.log(req.body.name);
+    pizza.name = req.body.name;
+    console.log(pizzaTypes);
+    // return the pizza so client will know the id
+    res.send(pizza);
+  }
+});
+
+// Use http://localhost:5000/api/delete_pizza/100
+pizzaApp.delete("/api/delete_pizza/:id", (req, res) => {
+  console.log("In pizza id:", req.params.id);
+  // const pizza = pizzaTypes.filter((p) => p.id === parseInt(req.params.id));
+  // if (pizza.length == 0) {
+  const pizza = pizzaTypes.find((p) => p.id === parseInt(req.params.id));
+  if (!pizza) {
+    res.status(404).send(`Pizza ${req.params.id} not found`);
+  } else {
+    pizzaIndex = pizzaTypes.indexOf(pizza);
+    pizzaTypes.splice(pizzaIndex, 1);
+    res.status(200).send(pizza);
+  }
 });
 
 module.exports = pizzaApp;
