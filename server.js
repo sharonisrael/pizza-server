4971680bcfc0d0aa671a281cd095b6e89cc49e48
Sharon Israel <avisar@hotmail.com>
2020-09-26 11:17:25 +0300

    get with params and  optional params

--- server.js
+++ server.js
@@ -13,13 +13,29 @@ app.get("/simple_array", (req, res) => {
 
 app.get("/get_all_pizzas", (req, res) => {
   res.status(200).json({
-    // IT's a JSON with several objects
-    // FIrst one is pizzas
+    // It's a JSON with several objects
+    // First one is pizzas
     pizzas: [1, 2, 3],
   });
 });
 
-const port = 3000;
+// Try with http://localhost:5000/api/posts/2020/1?sortyBy=Acs
+app.get("/api/posts/:year/:month", (req, res) => {
+  // res.send(req.params);
+  // res.send(req.query);
+  res.status(200).json({
+    params: req.params,
+    query: req.query,
+  });
+});
+
+// You can set process environment PORT using eith
+// export PORT=5000
+// or in Windows set PORT=5000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Listning on port", port);
+  // To create a template literal you need to use backticks next to the 1 key and not quote or single quotes
+  let browserLink = `http://localhost:${port}/`;
+  console.log("Use this link in the broweser " + browserLink);
 });
